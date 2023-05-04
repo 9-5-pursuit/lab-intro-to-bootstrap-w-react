7bec5aa846b46d96e5a5e9702bb190fff3a76d0e
Naomi-Pate <patenaomi99@gmail.com>
2023-05-04 12:03:22 -0400

    created static layout for Main View

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import postData from "./data/posts.json";
 
 function App() {
+  function posts() {}
+
+  function postsTableCount() {}
+
   console.log(postData);
-  return <main></main>;
+  return (
+    <main>
+      <nav>
+        <logo>Travel Blog</logo>
+        <a href="">About</a>
+        <a href="">Popular Posts</a>
+      </nav>
+      <div>
+        <h1>Travel is the only thing you buy that makes you richer...</h1>
+      </div>
+      <div>
+        <section>
+          <label>Search posts by title and location...</label>
+          <input type="text"></input>
+        </section>
+        <section>{posts()}</section>
+        <section>{postsTableCount()}</section>
+      </div>
+      <footer>
+        <a href="">Contact Me</a>
+        <a href="">About the Author</a>
+        <p>Lorem ipsum</p>
+      </footer>
+    </main>
+  );
 }
 
 export default App;

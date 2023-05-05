38c963dddf769b76c9d589acd7aabc387c166694
Jennifer <jenniferpeterson@pursuit.org>
2023-05-04 23:35:13 -0400

    🥺 PROJECT

---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+//Footer Component
+function Footer() {
+  return (
+    <footer className="card-footer">
+      <a href="www" className="m-3 link-dark text-decoration-none">
+        Contact
+      </a>
+      <a href="www" className="m-3 link-dark text-decoration-none">
+        About the author
+      </a>
+      <a href="www" className="m-3 link-dark text-decoration-none">
+        Copyright <span>MyTravelBlog</span>
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+//Header Component
+function Header() {
+  return (
+    <div className="col">
+      <h2 className="border-top border-bottom py-5 bg-light script-font text-center">
+        <span className="display-6">
+          Travel is the only thing you buy that makes you richer
+        </span>
+      </h2>
+    </div>
+  );
+}
+
+export default Header;

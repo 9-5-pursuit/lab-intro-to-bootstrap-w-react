2c67ddfd40f1ace3c403700bac7311aeeea5dd39
Chantal <chantalgomez@pursuit.org>
2023-05-07 20:49:15 -0400

    done right

--- src/Componets/Post.js
+++ src/Componets/Post.js
@@ -1,25 +1,25 @@
- export default function Post({item}) {
-    return (
-        <div className="card" style={{width: "18rem"}} key={item.id}>
-        <img src="..." className="card-img-top" alt="..."/>
-        <div className="card-body">
-          <h5 className="card-title">{item.title}</h5>
-          <p className="card-text">{item.content}</p>
-          <a href="#" className="btn btn-primary">Go to post</a>
-        </div>
-      </div>
+//  export default function Post({item}) {
+//     return (
+//         <div className="card" style={{width: "18rem"}} key={item.id}>
+//         <img src="..." className="card-img-top" alt="..."/>
+//         <div className="card-body">
+//           <h5 className="card-title">{item.title}</h5>
+//           <p className="card-text">{item.content}</p>
+//           <a href="#" className="btn btn-primary">Go to post</a>
+//         </div>
+//       </div>
     
-    );
-  }
+//     );
+//   }
   
 
-//   export default function BirdCard({bird, handleAddToCart}){
-//     return(
-//       <div className='card'>
-//         <h2>{bird.name}</h2>
-//         <p>Price: ${bird.amount}</p>
-//         <img src={bird.img} alt={""}/>
-//         <button onClick={()=>handleAddToCart(bird)}>Adpot</button>
-//       </div>
-//     )
-//   }
+// //   export default function BirdCard({bird, handleAddToCart}){
+// //     return(
+// //       <div className='card'>
+// //         <h2>{bird.name}</h2>
+// //         <p>Price: ${bird.amount}</p>
+// //         <img src={bird.img} alt={""}/>
+// //         <button onClick={()=>handleAddToCart(bird)}>Adpot</button>
+// //       </div>
+// //     )
+// //   }
--- src/Componets/Quote.js
+++ src/Componets/Quote.js
@@ -1,10 +1,10 @@
-export default function Quote(){
-    return(
-        <figure className="text-center bg-body-tertiary" >
-  <blockquote className="blockquote">
-    <p>Travel is the only thing you buy that makes you richer...</p>
-  </blockquote>
-</figure>
-    )
+// export default function Quote(){
+//     return(
+//         <figure className="text-center bg-body-tertiary" >
+//   <blockquote className="blockquote">
+//     <p>Travel is the only thing you buy that makes you richer...</p>
+//   </blockquote>
+// </figure>
+//     )
 
-}
+// }

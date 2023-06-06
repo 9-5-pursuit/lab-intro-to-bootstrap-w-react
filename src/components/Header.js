1564ebf0e051c673ef657a7e515ade7060864517
Mussie <mussierussom@pursuit.org>
2023-06-06 17:55:02 -0400

    Lab bootstrap is completed.

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+function Header() {
+  return (
+    <header id="header">
+      <nav className="navbar navbar-expand">
+        <div className="container">
+          <a className="navbar-brand fs-1 fw-bold" href="#">
+            Travel <span className="script-font text-warning">Blog</span>
+          </a>
+
+          <ul className="navbar-nav me-auto ms-5">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Popular posts
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/PostContainer/PostsList.js
@@ -0,0 +1,15 @@
+// // import postImages from "./images/index";
+// import PostCard from "./PostCard";
+
+// function PostsList({ posts }) {
+//   return (
+//     <div className="col-lg-8">
+//       <div className="row g-3 g-xxl-4">
+//         {posts.map((post) => (
+//           <PostCard key={post.id} {...post} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+// export default PostsList();
